79fc6573f5bafe09964b02e3d01bd76ae6b23e1e
helal uddin <helal@redlimesolutions.com>
2023-07-22 15:54:05 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,87 @@
+// FOR FAQ SECTION ACCORDION
+$(function ($) {
+  var panels = $(".faq__ans").hide();
+  panels.first().show();
+
+  $(".faq__que").click(function () {
+    var $this = $(this);
+    panels.slideUp();
+    $this.next().slideDown();
+  });
+});
+
+$(".faq__que").click(function () {
+  $(".faq__que i").addClass("bxs-chevron-right-circle");
+  $(this.children[1]).removeClass("bxs-chevron-right-circle");
+  $(this.children[1]).addClass("bxs-chevron-down-circle");
+});
+
+// =====================logo slider ============
+var swiper = new Swiper(".logoSlider", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  },
+});
+
+var swiper = new Swiper(".logoSlider2", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+// ============Blog=============
+var swiper = new Swiper(".blogSlider", {
+  slidesPerView: 2.9,
+  spaceBetween: 100,
+  initialSlide: "3",
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigator: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
